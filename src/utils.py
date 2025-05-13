946fe13693f2a6e2b5197bcfd2871894a9770968
AbdAziz1 <azzizz10301@gmail.com>
2025-05-13 15:48:29 +0500

    Data transformation added

---
+++ src/utils.py
@@ -0,0 +1,19 @@
+import sys
+import os
+import numpy as np
+import pandas as pd
+import dill
+from src.exception import CustomException
+
+def save_object(file_path, obj):
+    """
+    This function saves the given object to the specified file path using numpy's save function.
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'wb') as file:
+            dill.dump(obj, file)
+
+    except Exception as e:
+        raise CustomException(e, sys)
